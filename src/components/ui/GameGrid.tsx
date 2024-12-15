dc5769546df942f8fad992675a8910d001990735
Johannes Trahasch <johannes@rahasch.de>
2024-12-15 15:36:36 +0100

    Commit untracked files

--- src/components/ui/GameGrid.tsx
+++ src/components/ui/GameGrid.tsx
@@ -1,5 +1,6 @@
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import apiClient from "@/services/apiClient";
+import { Text } from "@chakra-ui/react";
 
 interface Game {
   id: number;
@@ -23,11 +24,14 @@ const GameGrid = () => {
   });
 
   return (
-    <ul>
-      {games.map((game) => (
-        <li key={game.id}>{game.name}</li>
-      ))}
-    </ul>
+    <>
+      {error && <Text>{error}</Text>}
+      <ul>
+        {games.map((game) => (
+          <li key={game.id}>{game.name}</li>
+        ))}
+      </ul>
+    </>
   );
 };
 
--- src/services/apiClient.ts
+++ src/services/apiClient.ts
@@ -4,5 +4,5 @@ export default axios.create({
     baseURL: 'https://api.rawg.io/api',
     params: {
         key: '79188eb4ff5e4adabcdbaac8a60c015e'
-    }
-})
+    },
+});
