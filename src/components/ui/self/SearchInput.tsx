0f310fa28c1c61e2badc4063c425ecadb7b18359
Johannes Trahasch <johannes@rahasch.de>
2024-12-16 00:06:10 +0100

    Implement search bar

--- src/components/ui/self/SearchInput.tsx
+++ src/components/ui/self/SearchInput.tsx
@@ -1,12 +1,29 @@
 import { Input } from "@chakra-ui/react";
 import { InputGroup } from "../input-group";
 import { LuSearch } from "react-icons/lu";
+import { useRef } from "react";
 
-const SearchInput = () => {
+interface Props {
+  onSearch: (searchText: string) => void;
+}
+
+const SearchInput = ({ onSearch }: Props) => {
+  const ref = useRef<HTMLInputElement>(null);
   return (
-    <InputGroup startElement={<LuSearch />}>
-      <Input borderRadius={20} placeholder="Search Games..."></Input>
-    </InputGroup>
+    <form
+      onSubmit={(event) => {
+        event.preventDefault();
+        if (ref.current) onSearch(ref.current.value);
+      }}
+    >
+      <InputGroup startElement={<LuSearch />}>
+        <Input
+          ref={ref}
+          borderRadius={20}
+          placeholder="Search Games..."
+        ></Input>
+      </InputGroup>
+    </form>
   );
 };
 
