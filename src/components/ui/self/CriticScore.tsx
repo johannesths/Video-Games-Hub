4d6b1de434d0ef533290364f30eeffce9532f1c5
Johannes Trahasch <johannes@trahasch.de>
2024-12-15 18:32:37 +0100

    Display critic score on game cards

---
+++ src/components/ui/self/CriticScore.tsx
@@ -0,0 +1,21 @@
+import { Badge } from "@chakra-ui/react";
+
+interface Props {
+  score: number;
+}
+
+const CriticScore = ({ score }: Props) => {
+  let color = score >= 75 ? "green" : score > 50 ? "yellow" : "red";
+  return (
+    <Badge
+      fontSize="14px"
+      paddingX="4px"
+      borderRadius="3px"
+      colorPalette={color}
+    >
+      {score}
+    </Badge>
+  );
+};
+
+export default CriticScore;
