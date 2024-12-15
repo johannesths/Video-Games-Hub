f5bd6a5a7e9ee0e8b72302a73ea0b9d710bb67ca
Johannes Trahasch <johannes@rahasch.de>
2024-12-15 19:02:18 +0100

    Adjust skeletons too fit actual size of game cards

--- src/components/ui/self/GameCardSkeleton.tsx
+++ src/components/ui/self/GameCardSkeleton.tsx
@@ -1,11 +1,10 @@
 import { Card, Skeleton } from "@chakra-ui/react";
-import React from "react";
 import { SkeletonText } from "../skeleton";
 
 const GameCardSkeleton = () => {
   return (
-    <Card.Root width="300px" borderRadius={10} overflow="hidden">
-      <Skeleton />
+    <Card.Root width="345px">
+      <Skeleton height="200px" />
       <Card.Body>
         <SkeletonText />
       </Card.Body>
