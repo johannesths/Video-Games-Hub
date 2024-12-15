78156264cf35b2b5acc12830bc63176509633c39
Johannes Trahasch <johannes@rahasch.de>
2024-12-15 20:05:13 +0100

    Display the genres and create genre skeletons

--- src/components/ui/self/GenreList.tsx
+++ src/components/ui/self/GenreList.tsx
@@ -1,13 +1,34 @@
 import useGenres from "@/hooks/useGenres";
+import getCroppedImageURL from "@/services/imageUrl";
+import { Box, HStack, Image, Text } from "@chakra-ui/react";
+import { SkeletonText } from "../skeleton";
 
 const GenreList = () => {
-  const { data } = useGenres();
+  const { data, isLoading } = useGenres();
+  if (isLoading) {
+    return (
+      <Box as="ul" padding="8px">
+        <SkeletonText noOfLines={20} height="32px" gap="2" />
+      </Box>
+    );
+  }
   return (
-    <ul>
-      {data.map((data) => (
-        <li key={data.id}>{data.name}</li>
+    <Box as="ul" padding="8px">
+      {data.map((genre) => (
+        <Box padding="5px">
+          <li key={genre.id}>
+            <HStack>
+              <Image
+                boxSize="32px"
+                borderRadius={8}
+                src={getCroppedImageURL(genre.image_background)}
+              />
+              <Text>{genre.name}</Text>
+            </HStack>
+          </li>
+        </Box>
       ))}
-    </ul>
+    </Box>
   );
 };
 
