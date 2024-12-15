adfca03ef88b441f25710aa5a5244c3b4eba72fd
Johannes Trahasch <johannes@rahasch.de>
2024-12-15 13:44:44 +0100

    Add a color mode switch

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,25 @@
-import { Grid, GridItem, Show } from "@chakra-ui/react";
+import { Grid, GridItem } from "@chakra-ui/react";
 import NavBar from "./components/ui/self/NavBar";
+import { ColorModeProvider } from "./components/ui/color-mode";
 
 function App() {
   return (
-    <Grid
-      templateAreas={{
-        base: `"nav" "main"`,
-        lg: `"nav nav" "aside main"`,
-      }}
-    >
-      <GridItem area="nav">
-        <NavBar></NavBar>
-      </GridItem>
-      <GridItem area="aside" hideBelow="lg">
-        ASIDE
-      </GridItem>
-      <GridItem area="main">MAIN</GridItem>
-    </Grid>
+    <ColorModeProvider>
+      <Grid
+        templateAreas={{
+          base: `"nav" "main"`,
+          lg: `"nav nav" "aside main"`,
+        }}
+      >
+        <GridItem area="nav">
+          <NavBar></NavBar>
+        </GridItem>
+        <GridItem area="aside" hideBelow="lg">
+          ASIDE
+        </GridItem>
+        <GridItem area="main">MAIN</GridItem>
+      </Grid>
+    </ColorModeProvider>
   );
 }
 
