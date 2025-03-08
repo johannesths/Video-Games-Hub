84f5d9d909e9bb7cd78ebfae6b75589cadf62728
Johannes Trahasch <147079222+johannesths@users.noreply.github.com>
2025-03-08 16:00:44 +0100

    Update API Client

--- src/services/apiClient.ts
+++ src/services/apiClient.ts
@@ -6,6 +6,6 @@ import axios from "axios";
 export default axios.create({
     baseURL: 'https://api.rawg.io/api',
     params: {
-        key: '79188eb4ff5e4adabcdbaac8a60c015e'
+        key: '?????'
     },
 });
